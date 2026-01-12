95197263c04b18a7a9b8acbacc67b895975e11de
Aditta Chakraborty <cseaditta@gmail.com>
2026-01-13 04:19:39 +0600

    delete a book api created

--- index.js
+++ index.js
@@ -371,6 +371,19 @@ async function run() {
         }
         );
 
+        // delete a book
+        app.delete("/books/:id", verifyFireBaseToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+
+            try {
+                const result = await booksCollection.deleteOne(query);
+                res.send(result);
+            } catch {
+                return res.status(400).send({ message: "Invalid book id" });
+            }
+        });
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
